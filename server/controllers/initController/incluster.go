0d91bc2f19ee4055e90ce4c4fd37feac2c3cc90d
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-18 19:27:41 +0800

    规范初始化程序配置代码

---
+++ server/controllers/initController/incluster.go
@@ -0,0 +1,48 @@
+package initcontroller
+
+import (
+	"context"
+	"server/utils/logs"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	newNamespace corev1.Namespace
+)
+
+func metaDataInit() {
+	logs.Info(nil, "初始化程序配置...")
+	// step1 初始化 kubeConfig 实例
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", "config/baseKubeConfig")
+	if err != nil {
+		logs.Error(map[string]any{"Error": err.Error()}, "初始化 kubeconfig 实例失败")
+		panic(err.Error())
+	}
+	// step2 创建客户端工具 clientSet
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		logs.Error(map[string]any{"Error": err.Error()}, "客户端工具 clientSet 创建失败")
+		panic(err.Error())
+	}
+	// step3 创建元数据 nameSpace
+	newNamespace.Name = "kc"
+	// 判断 nameSpace 是否已存在
+	_, err = clientset.CoreV1().Namespaces().Get(context.TODO(), newNamespace.Name, metav1.GetOptions{})
+	if err != nil {
+		logs.Warning(nil, "namespace: "+newNamespace.Name+" 不存在")
+		logs.Warning(nil, "开始创建 namespace: "+newNamespace.Name)
+		_, err = clientset.CoreV1().Namespaces().Create(context.TODO(), &newNamespace, metav1.CreateOptions{})
+		if err != nil {
+			logs.Error(map[string]any{"Error": err.Error()}, "namespace: "+newNamespace.Name+" 创建失败")
+			panic(err.Error())
+		}
+		logs.Warning(nil, "namespace: "+newNamespace.Name+" 创建成功")
+	} else {
+		logs.Warning(nil, "namespace: "+newNamespace.Name+" 已存在")
+	}
+	logs.Info(nil, "初始化程序配置已完成")
+}
